00e12392c61ac1a745b3a714c6073ea01d8e4641
refianys1678 <refianys1678@gmail.com>
2022-09-12 21:15:51 +0700

    submit tugas 2 ke github

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,9 @@
 # TODO: Implement Routings Here
+from django.urls import path
+from katalog.views import takeData
+
+app_name = 'katalog'
+
+urlpatterns = [
+    path('', takeData, name='takeData'),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
 # TODO: Create your views here.
+def takeData(request):
+    return render(request, 'katalog.html', context)
+
+data_katalog = CatalogItem.objects.all()
+context = {
+    'list_katalog': data_katalog,
+    'nama': 'Refiany Shadrina'
+}
