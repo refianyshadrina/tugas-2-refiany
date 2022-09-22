cd82d0f63a2e3a0129d368fe311738b4ba341b5e
refianys1678 <refianys1678@gmail.com>
2022-09-22 08:48:40 +0700

    push data updates

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
 
+# Create your tests here.
+from multiprocessing.connection import Client
+from django.test import TestCase, Client
+from django.urls import reverse
+
+class test(TestCase):
+    def testHtml(self):
+        client = Client()
+        response = client.get(reverse("mywatchlist:show_byhtml"))
+        self.assertEquals(response.status_code,200)
+
+    def testJSON(self):
+        client = Client()
+        response = client.get(reverse("mywatchlist:show_byjson"))
+        self.assertEquals(response.status_code,200)
+
+    def testXML(self):
+        client = Client()
+        response = client.get(reverse("mywatchlist:show_byxml"))
+        self.assertEquals(response.status_code,200)
 # Create your tests here.
